dd7b719776d3be6c891f80ee5462300fdc498a4f
cyrusng <cyrus.ng@protonmail.com>
2021-02-07 16:45:48 -0500

    Refactor

--- config/nvim/lua/lsp/common.lua
+++ config/nvim/lua/lsp/common.lua
@@ -1,57 +1,53 @@
-vim.api.nvim_buf_set_option(0, "omnifunc", "v:lua.vim.lsp.omnifunc")
-vim.api.nvim_set_option("updatetime", 300)
-vim.api.nvim_win_set_option(0, "signcolumn", "yes")
-vim.api.nvim_buf_set_keymap(0, 'n', 'gd', "<cmd>lua vim.lsp.buf.declaration()<cr>", {
-    noremap = true,
-    silent = true
-})
-vim.api.nvim_buf_set_keymap(0, 'n', 'gD', "<cmd>lua vim.lsp.buf.implementation()<cr>", {
-    noremap = true,
-    silent = true
-})
-vim.api.nvim_buf_set_keymap(0, 'n', '<c-]>', "<cmd>lua vim.lsp.buf.definition()<cr>", {
-    noremap = true,
-    silent = true
-})
-vim.api.nvim_buf_set_keymap(0, 'n', 'silent> <K>', "<cmd>lua vim.lsp.buf.buffer()<cr>", {
-    noremap = true,
-    silent = true
-})
-vim.api.nvim_buf_set_keymap(0, 'n', '<c-s>', "<cmd>lua vim.lsp.buf.signature_help()<cr>", {
-    noremap = true,
-    silent = true
-})
-vim.api.nvim_buf_set_keymap(0, 'n', '<c-t>', "<cmd>lua vim.lsp.buf.type_definition()<cr>", {
-    noremap = true,
-    silent = true
-})
-vim.api.nvim_buf_set_keymap(0, 'n', 'gr', "<cmd>lua vim.lsp.buf.references()<cr>", {
-    noremap = true,
-    silent = true
-})
-vim.api.nvim_buf_set_keymap(0, 'n', 'g0', "<cmd>lua vim.lsp.buf.document_symbol()<cr>", {
-    noremap = true,
-    silent = true
-})
-vim.api.nvim_buf_set_keymap(0, 'n', 'gW', "<cmd>lua vim.lsp.buf.workspace_symbol()<cr>", {
-    noremap = true,
-    silent = true
-})
-vim.api.nvim_buf_set_keymap(0, 'n', '<leader>rn', "<cmd>lua vim.lsp.buf.rename()<cr>", {
-    noremap = true,
-    silent = true
-})
-vim.api.nvim_buf_set_keymap(0, 'n', '<leader>qf', "<cmd>lua vim.lsp.buf.code_action()<cr>", {
-    noremap = true,
-    silent = true
-})
 
+-- Config from nvim-lsp config
 on_attach = function(client)
-    require'completion'.on_attach(client)
+    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+    local opts = { noremap=true, silent=true }
+    buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+    vim.api.nvim_set_option("updatetime", 300)
+    vim.api.nvim_win_set_option(0, "signcolumn", "yes")
+
+    buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    buf_set_keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    buf_set_keymap('n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+    buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+
+    if client.resolved_capabilities.document_formatting then
+        buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+    elseif client.resolved_capabilities.document_range_formatting then
+        buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+    end
+
+    -- Set autocommands conditional on server_capabilities
+    if client.resolved_capabilities.document_highlight then
+        vim.api.nvim_exec([[
+        hi LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
+        hi LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
+        hi LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
+        augroup lsp_document_highlight
+        autocmd!
+        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+        augroup END
+        ]], false)
+    end
+    require'completion_setup'
 end
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
+vim.lsp.diagnostic.on_publish_diagnostics, {
     -- This will disable virtual text, like doing:
     -- let g:diagnostic_enable_virtual_text = 0
     virtual_text = true,
@@ -65,5 +61,5 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     -- This is similar to:
     -- "let g:diagnostic_insert_delay = 1"
     update_in_insert = true,
-  }
+}
 )
