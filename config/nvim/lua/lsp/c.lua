7202f33e251ce2854e0668e0842396c24356bdce
cyrusng <cyrus.ng@protonmail.com>
2021-02-07 16:44:34 -0500

    Switch to clangd

--- config/nvim/lua/lsp/c.lua
+++ config/nvim/lua/lsp/c.lua
@@ -1,12 +1,15 @@
 require'lsp/common'
 
 local nvim_lsp = require'lspconfig'
-nvim_lsp.ccls.setup{
-    init_options = {
-        highlight = {
-            lsRanges = true
-        }
-    }; 
-    root_dir = nvim_lsp.util.root_pattern("compile_commands.json", "compile_flags.txt", ".ccls", ".git"); 
+nvim_lsp.clangd.setup{
     on_attach = on_attach;
 }
+-- nvim_lsp.ccls.setup{
+--     init_options = {
+--         highlight = {
+--             lsRanges = true
+--         }
+--     }; 
+--     root_dir = nvim_lsp.util.root_pattern("compile_commands.json", "compile_flags.txt", ".ccls", ".git"); 
+--     on_attach = on_attach;
+-- }
